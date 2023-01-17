40b6bd9df8ba75d90fb56046fbfae98efde19320
Chin.K <chinwat@odds.team>
2023-01-17 22:15:51 +0700

    rd

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,75 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useReducer } from "react";
 import "./App.css";
 
 interface Todo {
   text: string;
+  id: number;
 }
 
-function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [input, setInput] = useState("");
+interface TodoState {
+  todos: Todo[];
+  input: string;
+}
 
-  useEffect(() => {
-    let todoString = localStorage.getItem('todos')
-    if (todoString) {
-      setTodos(JSON.parse(todoString))
-    }
-  }, [])
+interface Action {
+  type: "ADD" | "UPDATE";
+  payload: Todo | string;
+}
+const initialState: TodoState = {
+  todos: JSON.parse(localStorage.getItem("todos") || "[]"),
+  input: "",
+};
+
+const todoReducer = (state: TodoState, action: Action) => {
+  switch (action.type) {
+    case "ADD":
+      localStorage.setItem(
+        "todos",
+        JSON.stringify([...state.todos, action.payload as Todo])
+      );
+      return {
+        ...state,
+        todos: [...state.todos, action.payload as Todo],
+        input: "",
+      };
+    case "UPDATE":
+      return {
+        ...state,
+        input: action.payload as string,
+      };
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const { todos, input } = state;
 
   const onClickAdd = () => {
-    const todo: Todo = { text: input };
-    const newTodoList = [...todos, todo];
-    setTodos(newTodoList);
-    setInput("");
-    storeInLocalStorage(newTodoList);
+    const newTodo: Todo = {
+      text: input,
+      id: Date.now(),
+    };
+    dispatch({ type: "ADD", payload: newTodo });
   };
 
-  const storeInLocalStorage = (newTodoList: Todo[]) => {
-    localStorage.setItem('todos', JSON.stringify(newTodoList))
-  }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: "UPDATE", payload: e.target.value });
+  };
 
   return (
     <div className="App">
       <div>
-        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <input value={input} onChange={handleChange} />
         <button onClick={onClickAdd}>Add Todo</button>
       </div>
 
       <div>
         <ul>
           {todos.map((todo, index) => {
-            return <TodoItem key={index} todo={todo} />
+            return <TodoItem key={index} todo={todo} />;
           })}
-          {/* key prop helps React keep track of each item in the list, so it knows which items have been added, removed, or changed. */}
         </ul>
       </div>
     </div>
@@ -53,10 +82,10 @@ interface TodoItemProps {
 
 const TodoItem = (props: TodoItemProps) => {
   const printTodo = useCallback(() => {
-    console.log('Todo: ' + props.todo.text)
-  }, [])
-  
-  return <li onClick={printTodo}>{props.todo.text}</li>
-}
+    console.log("Todo: " + props.todo.text);
+  }, [props.todo.text]);
+
+  return <li onClick={printTodo}>{props.todo.text}</li>;
+};
 
 export default App;
