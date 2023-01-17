35af5da60e9609aa9aead1dfb5ede443e1422f08
Chin.K <chinwat@odds.team>
2023-01-17 21:10:55 +0700

    binding input use state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+interface Todo {
+  text: string;
+}
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [input, setInput] = useState("");
+
+  const onClickAdd = () => {
+    const todo: Todo = { text: input };
+    setTodos([...todos, todo]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button onClick={onClickAdd}>Add Todo</button>
+      </div>
+
+      <div>
+        <ul>
+          {todos.map((todo, index) => {
+            return <li key={index}>{todo.text}</li>;
+          })}
+          {/* key prop helps React keep track of each item in the list, so it knows which items have been added, removed, or changed. */}
+        </ul>
+      </div>
     </div>
   );
 }
